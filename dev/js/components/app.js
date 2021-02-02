dff9e79c50a14d0f604860ff2ed49d8a82192996
tadeuasarro <tadeutas@gmail.com>
2021-02-02 16:58:51 -0300

    Video 6

---
+++ dev/js/components/app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+require('../../scss/style.scss');
+
+const App = () => (
+  <div>
+    <h2>Username List:</h2>
+    <hr />
+    <h2>User Details:</h2>
+  </div>
+);
+
+export default App;
